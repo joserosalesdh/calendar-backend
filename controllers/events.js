88437fdedecf43db33fa27069fd89f384e6bb74c
joserosalesdh <jm.rosales92@gmail.com>
2020-09-24 15:04:22 -0300

    eliminar evento

--- controllers/events.js
+++ controllers/events.js
@@ -48,7 +48,7 @@ const actualizarEvento = async (req, res = response) => {
         const evento = await Evento.findById(eventoId);
 
         if (!evento) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             });
@@ -83,12 +83,42 @@ const actualizarEvento = async (req, res = response) => {
 
 };
 
-const eliminarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'eliminar evento'
-    })
-};
+const eliminarEvento = async (req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de eliminar este evento'
+            })
+        }
+
+
+        await Evento.findByIdAndDelete(eventoId);
+
+        res.json({ ok: true });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el admin"
+        });
+    }
+}
 
 
 module.exports = {
